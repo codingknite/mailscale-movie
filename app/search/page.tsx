edb4305e7ee815e159eb3ffb645bada28d358073
Joel <itsjoelpm@gmail.com>
2024-06-01 20:09:07 +0300

    chore:  movie recommendations search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,50 +1,19 @@
-'use client';
+import SearchPage from '@/components/pages/Search/Search';
+import { MoviesApiResponse } from '@/types/movie';
 
-import FadeHero from '@/components/FadeHero';
-import MovieCard from '@/components/MovieCard';
-import { FormEvent } from 'react';
+const Search = async () => {
+  const apiKey = process.env.TMDB_API_KEY;
 
-const Search = () => {
-  const handleSearch = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
-
-  return (
-    <main className='text-white'>
-      <div>
-        <FadeHero imagePath='/search-poster.webp'>
-          <form
-            onSubmit={handleSearch}
-            className='w-[85%] mx-auto flex items-center justify-center md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[30%]'
-          >
-            <input
-              autoFocus
-              type='text'
-              placeholder='Avengers Endgame'
-              className='bg-gray-200 border-gray-300 border-[1.25px] w-full py-3 px-6 rounded-[2rem] outline-none font-normal text-base text-black'
-            />
-          </form>
-        </FadeHero>
-
-        <div className='p-6 2xl:w-[1440px] 2xl:mx-auto'>
-          <h2 className='text-white font-medium text-3xl py-3 mt-4'>
-            Recommended For You
-          </h2>
-
-          <div className='flex flex-wrap gap-2 ml-1 lg:gap-4 lg:ml-2 mt-4'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </div>
-      </div>
-    </main>
+  const fetchRecommendations = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=878`
   );
+
+  if (fetchRecommendations.ok) {
+    const recommendations: MoviesApiResponse =
+      await fetchRecommendations.json();
+
+    return <SearchPage data={recommendations.results} />;
+  }
 };
 
 export default Search;
