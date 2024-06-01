646bdb1598fe199cc941b849b997f6421b55235f
Joel <itsjoelpm@gmail.com>
2024-06-01 20:07:17 +0300

    chore: fetch movies homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,97 +1,51 @@
-import FadeHero from '@/components/FadeHero';
-import MovieCard from '@/components/MovieCard';
-import React from 'react';
+import HomePage from '@/components/pages/Home/Home';
+import { MovieProps } from '@/types/movie';
+import { baseApiURL } from '@/utils/helpers';
 
-const Home = () => {
-  return (
-    <main className='text-white'>
-      <div className='pt-14'>
-        <FadeHero imagePath='/spidey.webp'>
-          <div className='p-6 lg:ml-[5rem]'>
-            <h1 className='text-5xl w-[70%] font-medium md:w-[50%] md:text-6xl'>
-              Spider Man No Way Home
-            </h1>
-            <p className='text-base font-medium text-gray-200 py-4 w-[90%] md:w-[65%] lg:w-[50%]'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A quod
-              excepturi officiis optio nihil quia deserunt modi. Placeat, nihil
-              temporibus.
-            </p>
-            <button className='bg-red-600 py-2.5 px-8 rounded-[2rem] font-medium text-white text-base mt-8 outline-none'>
-              Read More
-            </button>
-          </div>
-        </FadeHero>
+interface ApiResponse {
+  page: number;
+  results: MovieProps[];
+  total_pages: number;
+  total_results: number;
+}
 
-        <div className='p-6 2xl:w-[1440px] 2xl:mx-auto'>
-          <h2 className='text-white font-medium text-2xl py-3 mt-4'>
-            My Favorites
-          </h2>
+const Home = async () => {
+  const apiKey = process.env.TMDB_API_KEY;
 
-          <div className='flex flex-wrap gap-2 ml-1 lg:gap-4 lg:ml-2 mt-8'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </div>
+  const fetchAll = {
+    popular: await fetch(
+      `${baseApiURL}/popular?api_key=${apiKey}&language=en-US&page=1`
+    ),
+    upcoming: await fetch(
+      `${baseApiURL}/upcoming?api_key=${apiKey}&language=en-US&page=1`
+    ),
+    nowPlaying: await fetch(
+      `${baseApiURL}/now_playing?api_key=${apiKey}&language=en-US&page=1`
+    ),
+  };
 
-        <div className='p-6 2xl:w-[1440px] 2xl:mx-auto'>
-          <h2 className='text-white font-medium text-2xl py-3 mt-4'>
-            My Favorites
-          </h2>
+  const [popular, upcoming, nowPlaying] = await Promise.all([
+    fetchAll.popular,
+    fetchAll.upcoming,
+    fetchAll.nowPlaying,
+  ]);
 
-          <div className='flex flex-wrap gap-2 ml-1 lg:gap-4 lg:ml-2 mt-8'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </div>
+  if (popular.ok && upcoming.ok && nowPlaying.ok) {
+    const popularMovies: ApiResponse = await popular.json();
+    const upcomingMovies: ApiResponse = await upcoming.json();
+    const nowPlayingMovies: ApiResponse = await nowPlaying.json();
 
-        <div className='p-6 2xl:w-[1440px] 2xl:mx-auto'>
-          <h2 className='text-white font-medium text-2xl py-3 mt-4'>
-            My Favorites
-          </h2>
-
-          <div className='flex flex-wrap gap-2 ml-1 lg:gap-4 lg:ml-2 mt-8'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </div>
-
-        <div className='p-6 2xl:w-[1440px] 2xl:mx-auto'>
-          <h2 className='text-white font-medium text-2xl py-3 mt-4'>
-            My Favorites
-          </h2>
-
-          <div className='flex flex-wrap gap-2 ml-1 lg:gap-4 lg:ml-2 mt-8'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+      <HomePage
+        featuredMovieIndex={Math.floor(Math.random() * 21)}
+        popular={popularMovies.results}
+        upcoming={upcomingMovies.results}
+        nowPlaying={nowPlayingMovies.results}
+      />
+    );
+  } else {
+    // todo: handle error
+  }
 };
 
 export default Home;
