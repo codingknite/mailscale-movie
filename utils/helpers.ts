0ad8d9ac566c21aa1e7682b15606f0ec06165931
Joel <itsjoelpm@gmail.com>
2024-06-01 20:17:17 +0300

    chore: helpers

---
+++ utils/helpers.ts
@@ -0,0 +1,3 @@
+export const baseImageURL = 'https://image.tmdb.org/t/p/w500';
+export const baseApiURL = 'https://api.themoviedb.org/3/movie';
+export const baseCastImageURL = 'https://image.tmdb.org/t/p/w500/';
